ec284955ebc33a4c25573b8bf73eea95693df0ac
jakubriegel <jakubriegel112@gmail.com>
2019-11-30 11:56:14 +0100

    boot 0.2.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.2.0.RELEASE"
+    id("org.springframework.boot") version "2.2.1.RELEASE"
     id("io.spring.dependency-management") version "1.0.8.RELEASE"
-    kotlin("jvm") version "1.3.50"
-    kotlin("plugin.spring") version "1.3.50"
+    kotlin("jvm") version "1.3.60"
+    kotlin("plugin.spring") version "1.3.60"
 }
 
 group = "pl.poznan.put.oculus"
-version = "0.0.2"
+version = "0.1.0"
 
 repositories {
     mavenCentral()
@@ -17,7 +17,7 @@ repositories {
 
 dependencies {
     // oculus
-    implementation("pl.poznan.put.oculus.boot:oculus-spring-boot-starter:0.2.2")
+    implementation("pl.poznan.put.oculus.boot:oculus-spring-boot-starter:0.2.3")
 
     // spring
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
@@ -28,8 +28,6 @@ dependencies {
     testImplementation("io.projectreactor:reactor-test")
     testImplementation("org.junit.jupiter:junit-jupiter:5.5.0")
     testImplementation("io.mockk:mockk:1.9.3")
-
-
 }
 
 tasks.withType<KotlinCompile> {
